395b9472c6c1825c674cae5ba81b78f2ec607a22
Ирина <mi.irene97@gmail.com>
2022-11-29 23:15:50 +0300

    Initial commit

---
+++ src/main/java/course3/lesson7_algorithms/Lesson37AlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package course3.lesson7_algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson37AlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson37AlgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course3/lesson7_algorithms/Lesson37AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package course3.lesson7_algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson37AlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
